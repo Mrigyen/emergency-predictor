c88eb28dbfd5a9ce4ec3f8a4ef49e78f2d119032
Mrigyen Sawant <Mrigyen@users.noreply.github.com>
2020-02-09 03:33:10 +0530

    implement GET

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request
 import model
 import encoding
 import proximity
@@ -15,7 +15,7 @@ def status() -> str:
     return status
 
 
-@app.route("/predict/<military_time>/<lat>/<longitude>/<age>/<gender>")
+@app.route("/old_predict/<military_time>/<lat>/<longitude>/<age>/<gender>")
 def predict(military_time, lat, longitude, age, gender) -> float:
     # get proximity score
     location_node = [float(lat), float(longitude)]
@@ -30,6 +30,34 @@ def predict(military_time, lat, longitude, age, gender) -> float:
     return str(proximity_score * prediction)
 
 
+@app.route("/predict", methods=["GET"])
+def get_predict():
+    if request.method == "GET":
+        military_time = request.args.get("military_time")
+        lat = request.args.get("lat")
+        longitude = request.args.get("long")
+        age = request.args.get("age")
+        gender = request.get("gender")
+        if gender == "male":
+            gender = 1
+        else:
+            gender = 0
+
+        # get proximity score
+        location_node = [float(lat), float(longitude)]
+        proximity_score = proximity.get_proximity(location_node)
+
+        # get prediction of emergency
+        sin_time = encoding.sin_time(encoding.military_time_in_minutes_fn(military_time))
+        cos_time = encoding.cos_time(encoding.military_time_in_minutes_fn(military_time))
+        prediction = model.predict(np.asarray([int(age), int(gender), sin_time, cos_time]).reshape(1, -1))
+
+        # return multiplication
+        return str(proximity_score * prediction)
+
+
 # run it directly via python3 main.py
 if __name__ == "__main__":
-    app.run()
+    hostname = "192.168.43.212"
+    port = 3005
+    app.run(host=hostname, port=port)
